fb9ba17d878e0e040b3506e2564bef54d46400ab
airflow <airflow@415926b19b26>
2025-03-07 06:58:07 +0000

    Updated Snowflake queries

--- MY_DBT_EXPERIMENT_DB/PUBLIC/Views/A_GRADE_STUDENTS_VIEW_FOR_NEW_DBSSSSSSSSSSSSSSS.sql
+++
@@ -1,8 +0,0 @@
-create or replace view A_GRADE_STUDENTS_VIEW_FOR_NEW_DBSSSSSSSSSSSSSSS(
-	FIRST_NAME,
-	LAST_NAME,
-	GRADE
-) as
-SELECT first_name, last_name, grade, 
-FROM MY_NEW_DB.MY_SCHEMA.what_is_student
-WHERE grade = 'C';
