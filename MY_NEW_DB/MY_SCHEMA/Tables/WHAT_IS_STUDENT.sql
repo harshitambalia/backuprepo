a95a666637db66e73e557336a4dffb5fe6549bad
airflow <airflow@415926b19b26>
2025-03-07 11:42:51 +0000

    Updated Snowflake queries

--- MY_NEW_DB/MY_SCHEMA/Tables/WHAT_IS_STUDENT.sql
+++
@@ -1,9 +0,0 @@
-create or replace TABLE WHAT_IS_STUDENT (
-	ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
-	FIRST_NAME VARCHAR(50),
-	LAST_NAME VARCHAR(50),
-	AGE NUMBER(38,0),
-	GRADE VARCHAR(2),
-	EMAIL VARCHAR(100),
-	primary key (ID)
-);
